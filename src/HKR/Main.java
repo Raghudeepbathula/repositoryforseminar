73cc2f4b3e7867b2489e66a62f6b54a6fae64c1f
raghudeepbathula <raghudeep.bathula98@gmail.com>
2019-12-03 00:22:14 +0100

    created a addition method

--- src/HKR/Main.java
+++ src/HKR/Main.java
@@ -3,6 +3,12 @@ package HKR;
 public class Main {
 
     public static void main(String[] args) {
+        int a =5;
+        int b =6;
+        int sum=a+b;
+        System.out.println(sum);
+
+
 
 
     }
